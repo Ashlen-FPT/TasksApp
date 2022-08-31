9b5478cac0393c7cc4592d36c8e67e7ee639441d
Ashlen-FPT <107978815+Ashlen-FPT@users.noreply.github.com>
2022-08-31 14:53:41 +0200

    Added New Log Table
    Log Implementation

---
+++ Enums/LogTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasksApp.Enums
+{
+    public static class LogTypes
+    {
+        public const string None = "None";
+        public const string Created = "Created";
+        public const string Updated = "Updated";
+        public const string Deleted = "Deleted";
+        public const string Completed = "Completed";
+    }
+}
